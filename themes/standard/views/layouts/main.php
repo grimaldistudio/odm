c2987b8e40561437fe0e8d8f0d8ce7f51b1984a0
root <root@imacdidomenico.station>
2015-12-10 11:54:55 +0100

    upgrade

--- themes/standard/views/layouts/main.php
+++ themes/standard/views/layouts/main.php
@@ -33,7 +33,7 @@
     
     <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
     <script type="text/javascript">
-        window.cookieconsent_options = {"message":"Utilizzando questo sito, acconsenti all'uso dei cookie, così come prescritto dalla vigente normativa","dismiss":"Accetto","learnMore":"Informativa","link":"/Note-Legali-Privacy","theme":"dark-bottom"};
+        window.cookieconsent_options = {"message":"Utilizzando questo sito, acconsenti all'uso dei cookie, così come prescritto dalla vigente normativa","dismiss":"Accetto","learnMore":"Informativa","link":<?php echo Yii::app()->createUrl('site/pageload', array('id' => 9))?>,"theme":"dark-bottom"};
     </script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
     <!-- End Cookie Consent plugin -->
