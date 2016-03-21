06477d2454fa98ddeb8644ed76d6b80e9a273eda
Domenico <Domenico@imacdidomenico.station>
2016-03-21 17:05:05 +0100

    some changes

--- themes/standard/views/dsAnagrafica/view.php
+++ themes/standard/views/dsAnagrafica/view.php
@@ -318,7 +318,7 @@ $this->pageTitle=Yii::app()->name;
                              if(isset($model->LOD)) {
                              $lod = explode("## Link ad altri dataset", $model->LOD);
                              
-                                     if(isset(CHtml::encode($lod[1]))) {
+                                     if((CHtml::encode($lod[1])!=null)) {
                                          echo CHtml::encode($lod[1]);
                                      }else{
                                          echo 'Questo dataset non contiene link ad altri dataset.';
