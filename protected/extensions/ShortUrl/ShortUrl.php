f199a7cc0cf385bcd7dc41ba3355d61598509bb1
Domenico <Domenico@imacdidomenico.station>
2016-03-30 11:04:54 +0200

    some changes

--- protected/extensions/ShortUrl/ShortUrl.php
+++ protected/extensions/ShortUrl/ShortUrl.php
@@ -107,7 +107,8 @@ class ShortUrl extends CApplicationComponent
 
 		// decode and return the JSON response
 		$json = json_decode($result, true);
-                if(count($json['error']['errors'])>0) return 'Si è verificato un problema. Url breve al momento non disponibile.';               
+                die(var_dump($json));
+                //if(count($json['error']['errors'])>0) return 'Si è verificato un problema. Url breve al momento non disponibile.';               
 		//return short link if available or false
 		return $json['id'] ? $json['id'] : false;
 	}
